66f92593be7a1863034072334525ecf81a75db25
habi202226 <80340484+habi202226@users.noreply.github.com>
2021-03-13 11:51:27 +0500

    Delete IDB.java

--- IDB.java
+++
@@ -1,8 +0,0 @@
-package com.company.model.database;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface IDB {
-    Connection getConnection() throws SQLException, ClassNotFoundException;
-}
